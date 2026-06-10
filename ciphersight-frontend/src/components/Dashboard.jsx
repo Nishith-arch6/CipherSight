07ababefb40ffcbbd603c37e73d2b64aa75a5f61
Nishith-arch6 <nishithgiriraju@gmail.com>
2026-06-10 13:39:58 +0530

    Switch map to light Google Maps-style theme using CartoDB Voyager tiles

--- ciphersight-frontend/src/components/Dashboard.jsx
+++ ciphersight-frontend/src/components/Dashboard.jsx
@@ -857,7 +857,7 @@ const simulateRogueDetection = () => {
           {/* 🗺️ ACTUAL LEAFLET MAP */}
           <div className="flex-1 w-full relative z-0">
             <MapContainer center={[12.9650, 77.5950]} zoom={14} style={{ height: '100%', width: '100%', background: '#020617' }} zoomControl={false}>
-              <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" />
+              <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png" attribution='&copy; <a href="https://carto.com/">CARTO</a>' />
               
               {['IDLE', 'RESPONDING'].includes(missionStatus) && (
                 <Polyline positions={routeToPatient} color="#eab308" weight={4} opacity={0.8} dashArray="5, 10" />
